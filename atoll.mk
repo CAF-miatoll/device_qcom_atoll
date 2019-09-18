5ce2f6a344bf02c907d6c2d86855b0f09d3f0378
Ramakant Singh <ramakant@codeaurora.org>
2019-09-18 11:07:21 +0530

    atoll: Update dalvik heap parameters.
    Change-Id: I1d47798706d52f3d3c3de18879db873d048577f5

--- atoll.mk
+++ atoll.mk
@@ -1,6 +1,14 @@
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 
+TARGET_DEFINES_DALVIK_HEAP := true
+#Inherit all except heap growth limit from phone-xhdpi-2048-dalvik-heap.mk
+RODUCT_PROPERTY_OVERRIDES  += \
+   dalvik.vm.heapstartsize=8m \
+   dalvik.vm.heapsize=512m \
+   dalvik.vm.heaptargetutilization=0.75 \
+   dalvik.vm.heapminfree=512k \
+   dalvik.vm.heapmaxfree=8m
 
 # Enable chain partition for system, to facilitate system-only OTA in Treble.
 BOARD_AVB_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
