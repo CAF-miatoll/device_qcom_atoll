54602d01a3b74d8d0be6429748a6364cf580e359
Linyu Yuan <linyyuan@codeaurora.org>
2020-05-06 17:15:36 +0800

    atoll: remove fingerprint to fix CTS issue
    fingerprint default is not support.
    
    Change-Id: Ide96afb97cd018785f5743c20f3a4c9200a62ee9
    Signed-off-by: Linyu Yuan <linyyuan@codeaurora.org>

--- atoll.mk
+++ atoll.mk
@@ -193,10 +193,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-service \
     libhealthd.msm
 
-# Fingerprint feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
-
 #ANT+ stack
 PRODUCT_PACKAGES += \
     AntHalService \
@@ -235,9 +231,6 @@ PRODUCT_PACKAGES += \
 # MIDI feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
