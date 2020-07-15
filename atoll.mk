3f18f3abb4df9a6e4bea41fcfef241f3b74300b4
Vinoth Jayaram <c_vinoja@codeaurora.org>
2020-07-15 17:16:08 +0530

    atoll: Updated Display HALs
    Removed HIDL Light and configstore Hal
    Enabled Light AIDL HAL
    
    Change-Id: Idd7cea7fc1c9777b0c9a5882aec6ed6140b6530e

--- atoll.mk
+++ atoll.mk
@@ -202,7 +202,6 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_HOST_PACKAGES += \
@@ -280,6 +279,10 @@ ENABLE_VENDOR_RIL_SERVICE := true
 PRODUCT_PROPERTY_OVERRIDES += \
 			ro.crypto.volume.filenames_mode = "aes-256-cts" \
 			ro.crypto.allow_encrypt_override = true
+
+#Enable Light AIDL HAL
+PRODUCT_PACKAGES += android.hardware.lights-service.qti
+
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
